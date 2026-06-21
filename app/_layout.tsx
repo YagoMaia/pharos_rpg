8ae95cb684e8c4b07eed33ee8ad868496447e77c
Joao Victor Cardozo <danichs1one@gmail.com>
2026-06-21 18:18:21 -0300

    feat: restructure player navigation and extract sheet routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@
 import { AlertProvider } from "@/context/AlertContext";
 import { CampaignProvider } from "@/context/CampaignContext"; 
 import { CharacterProvider } from "@/context/CharacterContext";
+import { PlayerProvider } from "@/context/PlayerContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { WebSocketProvider } from "@/context/WebSocketContext";
 import { Stack } from "expo-router";
@@ -11,15 +12,17 @@ export default function RootLayout() {
     <ThemeProvider>
       <AlertProvider>
         <CharacterProvider>
-          <CampaignProvider>
-            <WebSocketProvider>
-              <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
-                <Stack.Screen name="(player)" />
-                <Stack.Screen name="(gm)" />
-              </Stack>
-            </WebSocketProvider>
-          </CampaignProvider>
+          <PlayerProvider>
+            <CampaignProvider>
+              <WebSocketProvider>
+                <Stack screenOptions={{ headerShown: false }}>
+                  <Stack.Screen name="index" />
+                  <Stack.Screen name="(player)" />
+                  <Stack.Screen name="(gm)" />
+                </Stack>
+              </WebSocketProvider>
+            </CampaignProvider>
+          </PlayerProvider>
         </CharacterProvider>
       </AlertProvider>
     </ThemeProvider>
